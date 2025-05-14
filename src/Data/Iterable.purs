12186d419f30b89e310f758ac68b4e8041b188ab
Ben Neiswander <ben.neiswander@gmail.com>
2025-05-14 13:45:27 -0500

    Replaces Effect with ST

--- src/Data/Iterable.purs
+++ src/Data/Iterable.purs
@@ -1,51 +1,59 @@
 module Data.Iterable
-  ( class Iterable, iterator, unsafeIterator, Iterator, next, traverse_, toArray
+  ( class Iterable
+  , iterator
+  , unsafeIterator
+  , Iterator
+  , next
+  , traverse_
+  , toArray
   ) where
 
-import Prelude (Unit, (<$), unit, (>>=), pure, void, bind, discard, (<$>), otherwise)
-import Data.Maybe (Maybe (..), maybe)
+import Control.Monad.ST.Uncurried
+
+import Control.Monad.Rec.Class (tailRecM, Step(..))
+import Control.Monad.ST (ST)
+import Control.Monad.ST.Internal as ST
 import Data.Array (snoc) as Array
-import Control.Monad.Rec.Class (tailRecM, Step (..))
+import Data.Maybe (Maybe(..), maybe)
 import Foreign (Foreign, unsafeFromForeign)
-import Effect (Effect)
-import Effect.Uncurried (EffectFn1, runEffectFn1)
-import Effect.Ref (new, modify, read) as Ref
+import Prelude (Unit, (<$), unit, (>>=), pure, void, bind, discard, (<$>), otherwise)
 
 -- | The Iterable class corresponds to the Iterable interface in JS.
 -- | Instances should have a canonical `Iterator` returned.
 class Iterable iterable value | iterable -> value where
   -- | Returns the `Iterator` from an `Iterable`
-  iterator :: iterable -> Iterator value
+  iterator :: forall r. iterable -> ST r (Iterator value)
+
 instance iterableArray ∷ Iterable (Array a) a where
   iterator = unsafeIterator
 
 -- | Assume the `iterable` value has a property with the `Symbol.iterator` as the key, which is a nullary function
 -- | returning the `Iterator`.
-foreign import unsafeIterator :: forall iterable value. iterable -> Iterator value
+foreign import unsafeIterator :: forall iterable value r. iterable -> ST r (Iterator value)
 
 -- | Corresponds to an `Iterator` object in JS.
 foreign import data Iterator :: Type -> Type
 
-foreign import nextImpl :: forall value. EffectFn1 (Iterator value) { value :: Foreign, done :: Boolean }
+foreign import nextImpl :: forall value r. STFn1 (Iterator value) r { value :: Foreign, done :: Boolean }
 
 -- | `next()` is an effectful function, treating a `Iterator value` similarly to a `Ref`.
-next :: forall value. Iterator value -> Effect (Maybe value)
-next i = toMaybe <$> runEffectFn1 nextImpl i
+next :: forall value r. Iterator value -> ST r (Maybe value)
+next i = toMaybe <$> runSTFn1 nextImpl i
   where
-    toMaybe {done,value}
-      | done = Nothing
-      | otherwise = Just (unsafeFromForeign value)
+  toMaybe { done, value }
+    | done = Nothing
+    | otherwise = Just (unsafeFromForeign value)
 
 -- | Consumes all values in the `Iterator`.
-traverse_ :: forall value. (value -> Effect Unit) -> Iterator value -> Effect Unit
+traverse_ :: forall value r. (value -> ST r Unit) -> Iterator value -> ST r Unit
 traverse_ f i = tailRecM go unit
   where
-    go :: Unit -> Effect (Step Unit Unit)
-    go _ = next i >>= \mv -> maybe (pure (Done unit)) (\x -> Loop unit <$ f x) mv
+  go :: Unit -> ST r (Step Unit Unit)
+  go _ = next i >>= \mv -> maybe (pure (Done unit)) (\x -> Loop unit <$ f x) mv
 
 -- | Consumes all values in the `Iterator`, and turns it into an array.
-toArray :: forall value. Iterator value -> Effect (Array value)
+toArray :: forall value r. Iterator value -> ST r (Array value)
 toArray i = do
-  xsRef <- Ref.new []
-  traverse_ (\x -> void (Ref.modify (\ys -> ys `Array.snoc` x) xsRef)) i
-  Ref.read xsRef
+  xsRef <- ST.new []
+  traverse_ (\x -> void (ST.modify (\ys -> ys `Array.snoc` x) xsRef)) i
+  ST.read xsRef
